8fa73a19708d4709539d0b44c8582ed48ef40456
Denys Vuika <denys.vuika@gmail.com>
2018-09-25 14:09:36 +0100

    setup extension

--- projects/my-extension/src/lib/my-extension.module.ts
+++ projects/my-extension/src/lib/my-extension.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
+import { ExtensionService } from '@alfresco/adf-extensions';
+
 import { MyExtensionComponent } from './my-extension.component';
 
 @NgModule({
-  imports: [
-  ],
+  imports: [],
   declarations: [MyExtensionComponent],
   exports: [MyExtensionComponent]
 })
-export class MyExtensionModule { }
+export class MyExtensionModule {
+  constructor(extensions: ExtensionService) {
+    extensions.setComponents({
+      'my-extension.main.component': MyExtensionComponent
+    });
+  }
+}
