34e8013a4f4bf1efc1302242c0c7664ec75117aa
Denys Vuika <denys.vuika@gmail.com>
2018-09-25 13:52:49 +0100

    extension example scaffold

---
+++ projects/my-extension/src/lib/my-extension.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { MyExtensionComponent } from './my-extension.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [MyExtensionComponent],
+  exports: [MyExtensionComponent]
+})
+export class MyExtensionModule { }
